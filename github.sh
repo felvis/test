0d1d2c1d3ea3680e00aa37a4ed7b415e3bf1cd09
Oleg Zavorin <felvisua@gmail.com>
2018-06-16 00:21:50 +0300

    Update github.sh

--- github.sh
+++ github.sh
@@ -12,11 +12,12 @@ CODEBUILD_BUILD_ARN=$4
 
 BUILD_ID=`echo $CODEBUILD_BUILD_ARN | cut -d"/" -f2- | cut -d":" -f2`
 PL_NUMBER=`echo $CODEBUILD_SOURCE_VERSION | cut -c 4-`
-S3_LINK="https://s3.amazonaws.com/test7777khkn/$BUILD_ID/artifacts.zip"
+
 
 if [ "$CODEBUILD_BUILD_SUCCEEDING" -eq 0 ]; then 
  curl -X POST "https://api.github.com/repos/felvis/test/pulls/$PL_NUMBER/reviews" -H "authorization: Bearer $TOKEN" -H "content-type: application/json" -d '{"body":"Need to be fixed", "event":"REQUEST_CHANGES"}'; 
  else
  #curl -X PUT "https://api.github.com/repos/felvis/test/pulls/$PL_NUMBER/merge" -H "authorization: Bearer $TOKEN" -H "content-type: application/json" -d '{"commit_title":"TEST1", "commit_message":"TEST2"}';
+ S3_LINK='https://s3.amazonaws.com/test7777khkn/$BUILD_ID/artifacts.zip'
  curl -X POST "https://api.github.com/repos/felvis/test/pulls/$PL_NUMBER/reviews" -H "authorization: Bearer $TOKEN" -H "content-type: application/json" -d '{"body":"APPROVED. Link to artifacts - $S3_LINK", "event":"APPROVE"}'; 
  fi
